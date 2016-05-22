f806288608fdff4b6f2caeb99cc3cb0c80d18157
jleung51 <jleung513@gmail.com>
2016-05-22 16:18:55 -0700

    Remove extra brace preventing compilation

--- arduino/src/car_system/car_system.ino
+++ arduino/src/car_system/car_system.ino
@@ -103,7 +103,6 @@ void loop() {
       String in = ppad.readStringUntil('\n');
       alertSensorEssential = (in.charAt(0) == '1');
       alertSensorGeneral = (in.charAt(1) == '1');
-      }
     }
   }
 
